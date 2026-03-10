d077042cbfc67a0efd51ee63469b857a33d9540b
gorgi_64 <photographerjorkopeisct@gmail.com>
2026-03-10 12:52:14 +0200

    fixed recursion

--- March/1003/src/MDRSquare.java
+++ March/1003/src/MDRSquare.java
@@ -5,12 +5,28 @@ public class MDRSquare {
         if(matrix.length == 2) return MDRfirst.matrixSorP(matrix, 0);
         return MDRfirst.matrixSorP(matrix, 0) -
                 findMatrixSumRecursive(MDRPloshtadka.subMatrix(matrix,
-                        1, matrix.length - 2,
-                        1, matrix[0].length - 2));
+                        1, 1,
+                        matrix.length - 2, matrix[0].length - 1));
     }
+    /*public static void printRecursive(int[][] matrix){
+        if(matrix.length == 0 || matrix[0].length == 0) return;
+        if(matrix.length == 1){
+            System.out.println(matrix[0][0]);
+            return;
+        }
+        if(matrix.length == 2){
+            for(int[] that : matrix) PurvaIPosledna.print(that);
+            System.out.print("\n");
+            return;
+        }
+        for(int[] that : matrix) PurvaIPosledna.print(that);
+        System.out.print("\n");
+        printRecursive(MDRPloshtadka.subMatrix(matrix, 1, 1, matrix.length - 2, matrix[0].length - 1));
+
+    }*/
     public static void main(String[]  args) {
         int M = MDRfirst.input.nextInt();
-        int matrix[][] = MDRfirst.fillMatrix(M, M);
-            System.out.println(findMatrixSumRecursive(matrix));
+        int[][] matrix = MDRfirst.fillMatrix(M, M);
+        System.out.println(findMatrixSumRecursive(matrix));
     }
 }
