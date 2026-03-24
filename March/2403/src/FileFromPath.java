daa9df766d3fff9393f51d168b3c861be90f6fba
gorgi_64 <photographerjorkopeisct@gmail.com>
2026-03-24 08:33:35 +0200

    removed try/catch for this task

--- March/2403/src/FileFromPath.java
+++ March/2403/src/FileFromPath.java
@@ -1,9 +1,7 @@
 public class FileFromPath {
     public static String findNameRecursive(String that, char delimiter){
         if(that.indexOf(delimiter) == -1) return that;
-        try {
-            return findNameRecursive(that.substring(that.indexOf(delimiter) + 1), delimiter);
-        }catch(StringIndexOutOfBoundsException e){ return that; }
+        return findNameRecursive(that.substring(that.indexOf(delimiter) + 1), delimiter);
     }
     public static void main(String[] args) {
         System.out.println(findNameRecursive(args[0], '/'));
